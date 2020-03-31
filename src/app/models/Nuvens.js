ccd620b4d18afedcde8291007dbda3caffe85bb8
Jeniffer Carvalho <jenicarvalho@live.com>
2020-03-31 12:51:36 -0300

    adicionado comentarios, funcao com novo dia e retorno objeto do terceiro dia

--- src/app/models/Nuvens.js
+++ src/app/models/Nuvens.js
@@ -7,6 +7,42 @@ class Nuvens {
     this.matrix = this.matrixBase()
   }
 
+  newDay = (item, index, matrix) => {
+    // item = conteudo; index = indice; matrix = matriz original
+
+    // verifica se é primeira linha  
+    // se o index é maior do a largura da area (9)
+    // pego o indice atual, volto uma linha subtraindo a largura da area do indice
+    // comparado se o valor do item daquele indice é N
+    if (index > this.landArea - 1 && matrix[index - this.landArea] === 'N') {
+      return 'N'
+    }
+
+    // verifica se é a ultima linha  
+    // se o index é menor que o tamanho do array - a largura da area(linha)
+    // pego o indice atual, avanço uma linha adicionando a largura da area ao indice
+    // comparado se o valor do item daquele indice é N
+    if (index < (matrix.length - this.landArea) && matrix[index + this.landArea] === 'N') {
+      return 'N'
+    }
+
+    // verifico se não é um index da primeira coluna
+    // evita adicionar N no primeiro item da linha inferior
+    // comparo se o valor anterior é N
+    if (index % this.landArea !== 0 && matrix[index - 1] === 'N') {
+      return 'N'
+    }
+
+    // verifica se não é o último indice da linha
+    // comparo se o próximo valor é N
+    if (index % (this.landArea - 1) !== 0 && matrix[index + 1] === 'N') {
+      return 'N'
+    }
+
+    // retorno tudo
+    return item
+  }
+
   matrixBase = () => {
     let matrix = [
       'N', '.', '.', '.', '.', '.', '.', '.', '.', '.',
@@ -18,40 +54,16 @@ class Nuvens {
       '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
       '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
       '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
-      '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'
+      '.', '.', '.', '.', '.', '.', '.', '.', '.', 'N'
     ]
 
-    const newMatrix = matrix.map((item, index, matrix) => {
-
-      // verifica se é primeira linha e é maior o tamanho da land (9) - verifica se a linha superior é N
-      if (index > this.landArea - 1 && matrix[index - this.landArea] === 'N') {
-        return 'N'
-      }
-
-      // verifica se é a ultima linha e verifica se a linha inferior é N
-      if (index < matrix.length - this.landArea && matrix[index + this.landArea] === 'N') {
-        return 'N'
-      }
-
-      // verifica se o indice anterior é N
-      if (matrix[index - 1] === 'N') {
-        if (index % this.landArea !== 0) {
-          return 'N'
-        }
-      }
-
-      // verifica se o proximo item é N
-      if ((index === 1 || (index + 1) % this.landArea !== 0) && matrix[index + 1] === 'N') {
-        return 'N'
-      }
-
-      // retorno tudo
-      return item
-    })
+    const dayTwo = matrix.map(this.newDay)
+    const dayThree = dayTwo.map(this.newDay)
 
     return {
       dayOne: matrix,
-      dayTwo: newMatrix
+      dayTwo,
+      dayThree
     }
   }
 }
